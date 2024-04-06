62802041f1dd100392879ce04c79fd4d92383e34
Mohit Bhatt <bhattmohit869@gmail.com>
2024-04-06 08:53:46 +0530

    deployment changes

--- Backend/index.js
+++ Backend/index.js
@@ -11,36 +11,36 @@ dotenv.config();
 const allowedOrigin = process.env.BASE_URL;
 console.log(allowedOrigin);
 
-// app.use(cors({
-//   origin: process.env.BASE_URL,
-//   optionsSuccessStatus: 200,
-// }));
-// app.use(cors());
-// app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", allowedOrigin);
-//   res.header(
-//     "Access-Control-Allow-Methods",
-//     "GET, HEAD, OPTIONS, POST, PUT, DELETE"
-//   );
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-//   );
-//   next();
-// });
 app.use(cors({
-  origin : (origin ,callback)=>{
-    if(allowedOrigin.includes(origin)){
-      console.log(origin ,allowedOrigin);
-      callback(null,true);
-    }
-    else{
-      callback(new Error("Not allowed by CORS"))
-    }
-  },
-  credentials : true,
-  methods : ["GET", "POST", "PUT", "DELETE"],
+  origin: process.env.BASE_URL,
+  optionsSuccessStatus: 200,
 }));
+app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", allowedOrigin);
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, HEAD, OPTIONS, POST, PUT, DELETE"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
+});
+// app.use(cors({
+//   origin : (origin ,callback)=>{
+//     if(allowedOrigin.includes(origin)){
+//       console.log(origin ,allowedOrigin);
+//       callback(null,true);
+//     }
+//     else{
+//       callback(new Error("Not allowed by CORS"));
+//     }
+//   },
+//   credentials : true,
+//   methods : ["GET", "POST", "PUT", "DELETE"],
+// }));
 connectDB();
 app.use(express.json({ strict: false }));
 app.use(express.urlencoded({ extended: false }));
